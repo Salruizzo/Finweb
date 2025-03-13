2543ab9b056a7d8d409e16d8316fae78c03c5981
Salruizzo <137157141+Salruizzo@users.noreply.github.com>
2025-03-13 01:06:09 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple JavaScript functionality
+console.log("Mock FinViz loaded.");
+
+// Future enhancements could include dynamic data fetching,
+// chart integrations, or interactive table features.
